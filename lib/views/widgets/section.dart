208d4a2ea1f26c7ad75debff16a668fcbc22238c
Mahmoud Khalid <darkness20016@gmail.com>
2023-06-08 19:02:06 +0200

    Custom section upgrade

--- lib/views/widgets/section.dart
+++ lib/views/widgets/section.dart
@@ -2,33 +2,40 @@ import 'package:flutter/material.dart';
 
 import '../../utils/constants.dart';
 
+enum SectionLayout { column, row, wrap }
+
 class CustomSection extends StatelessWidget {
   const CustomSection({
     this.stretch = false,
-    this.direction = Axis.horizontal,
-    this.alignment = WrapAlignment.center,
-    this.padding = AppDecoration.paddingMedium,
-    this.spacing = AppDecoration.spaceLarge,
-    this.height = AppDecoration.sectionHeight,
+    this.height,
+    this.padding,
     this.borderRadius,
     this.backgroundColor,
+    this.wrapSpacing = AppDecoration.spaceLarge,
+    this.wrapCrossAlignment,
+    this.crossAxisAlignment,
+    this.mainAxisAlignment,
+    this.layout = SectionLayout.column,
     required this.children,
     super.key,
   });
 
   final bool stretch;
-  final Axis direction;
-  final WrapAlignment alignment;
-  final double padding;
-  final double spacing;
-  final double height;
+  final double? height;
+  final EdgeInsets? padding;
   final BorderRadiusGeometry? borderRadius;
   final Color? backgroundColor;
+  final double wrapSpacing;
+  final WrapCrossAlignment? wrapCrossAlignment;
+  final CrossAxisAlignment? crossAxisAlignment;
+  final MainAxisAlignment? mainAxisAlignment;
+  final SectionLayout layout;
   final List<Widget> children;
 
   @override
   Widget build(BuildContext context) {
     BoxDecoration? decoration;
+    final Widget layoutWidget;
 
     if (backgroundColor != null) {
       decoration = BoxDecoration(
@@ -37,23 +44,46 @@ class CustomSection extends StatelessWidget {
       );
     }
 
+    switch (layout) {
+      case SectionLayout.column:
+        layoutWidget = Column(
+          mainAxisAlignment: mainAxisAlignment ?? MainAxisAlignment.center,
+          crossAxisAlignment: crossAxisAlignment ?? CrossAxisAlignment.center,
+          children: children,
+        );
+        break;
+      case SectionLayout.row:
+        layoutWidget = Row(
+          mainAxisAlignment: mainAxisAlignment ?? MainAxisAlignment.center,
+          crossAxisAlignment: crossAxisAlignment ?? CrossAxisAlignment.center,
+          children: children,
+        );
+        break;
+      case SectionLayout.wrap:
+        layoutWidget = Wrap(
+          alignment: WrapAlignment.center,
+          runAlignment: WrapAlignment.center,
+          crossAxisAlignment: wrapCrossAlignment ?? WrapCrossAlignment.center,
+          spacing: wrapSpacing,
+          runSpacing: wrapSpacing,
+          children: children,
+        );
+        break;
+    }
+
     return Container(
-      padding: EdgeInsets.all(padding),
+      padding: padding ??
+          const EdgeInsets.symmetric(
+            horizontal: AppDecoration.paddingBig,
+            vertical: 70.0,
+          ),
       decoration: decoration,
       constraints: BoxConstraints(
         minWidth: stretch ? double.infinity : AppDecoration.pageWidth,
         maxWidth: stretch ? double.infinity : AppDecoration.pageWidth,
-        minHeight: height,
+        minHeight: height ?? 0,
       ),
-      child: Wrap(
-        alignment: alignment,
-        runAlignment: WrapAlignment.center,
-        crossAxisAlignment: WrapCrossAlignment.center,
-        direction: direction,
-        spacing: spacing,
-        runSpacing: spacing,
-        children: children,
-      ),
+      child: layoutWidget,
     );
   }
 }
