f65493b524535403d299362a120379082c262d6b
Mahmoud Khalid <darkness20016@gmail.com>
2023-06-17 11:34:53 +0200

    Added fetcher function

---
+++ lib/utils/fetcher.dart
@@ -0,0 +1,12 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+Future<String> fetchString(String apiURL) async {
+  final http.Response response = await http.get(Uri.parse(apiURL));
+  return response.body;
+}
+
+Future<List<Map<String, dynamic>>> fetchList(String apiURL) async {
+  return jsonDecode(await fetchString(apiURL)).cast<Map<String, dynamic>>();
+}
