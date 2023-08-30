34a477cbcef94af560c0249d28ba96c12274e0a6
MahmoudKhalid <darkness20016@gmail.com>
2023-08-30 19:56:37 +0200

    Space adjusting for TokenFeaturesSection

--- lib/views/token/features_section.dart
+++ lib/views/token/features_section.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:line_icons/line_icons.dart';
 
+import '../../utils/constants.dart';
 import '../widgets/feature.dart';
 import '../widgets/section.dart';
 
@@ -11,38 +12,39 @@ class TokenFeaturesSection extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     const double width = 350.0;
-    const double height = 200.0;
 
     return CustomSection(
+      padding: const EdgeInsets.only(
+        top: 100.0,
+        bottom: AppDecoration.paddingBig,
+        right: AppDecoration.paddingBig,
+        left: AppDecoration.paddingBig,
+      ),
       layout: SectionLayout.wrap,
       children: [
         CustomFeature(
-          icon: LineIcons.ban,
+          icon: LineIcons.alternateShield,
           title: "1002@token".tr,
           description: "1003@token".tr,
           width: width,
-          height: height,
         ),
         CustomFeature(
           icon: LineIcons.coins,
           title: "1004@token".tr,
           description: "1005@token".tr,
           width: width,
-          height: height,
         ),
         CustomFeature(
           icon: LineIcons.recycle,
           title: "1006@token".tr,
           description: "1007@token".tr,
           width: width,
-          height: height,
         ),
         CustomFeature(
-          icon: LineIcons.alternateShield,
+          icon: LineIcons.wiredNetwork,
           title: "1008@token".tr,
           description: "1009@token".tr,
           width: width,
-          height: height,
         ),
       ],
     );
