a5d5a93bf762625a7af5028f97c330e428a8bdc4
MahmoudKhalid <darkness20016@gmail.com>
2023-09-11 17:00:20 +0200

    Hide beta section

--- lib/views/download/page.dart
+++ lib/views/download/page.dart
@@ -4,7 +4,6 @@ import '../widgets/page.dart';
 import 'platforms_section.dart';
 import 'main_section.dart';
 import 'rate_section.dart';
-import 'beta_section.dart';
 
 class DownloadView extends StatelessWidget {
   const DownloadView({super.key});
@@ -13,7 +12,6 @@ class DownloadView extends StatelessWidget {
   Widget build(BuildContext context) {
     return CustomPage(const [
       MainDownloadSection(),
-      BetaSection(),
       PlatformsSection(),
       RateSection(),
     ]);
