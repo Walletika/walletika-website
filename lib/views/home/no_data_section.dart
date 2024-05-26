dd90596e85e03399767c0158826ef9bcf9426654
MahmoudKhalid <darkness20016@gmail.com>
2024-05-26 02:13:19 +0200

    NoDataSection removed background and icon

--- lib/views/home/no_data_section.dart
+++ lib/views/home/no_data_section.dart
@@ -1,98 +1,49 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:line_icons/line_icons.dart';
 
 import '../../utils/constants.dart';
 import '../widgets/section.dart';
 import '../widgets/spacer.dart';
 import '../widgets/text.dart';
 
-class NoDataSection extends GetResponsiveView {
-  NoDataSection({
-    super.settings = AppDecoration.responsiveScreenSettings,
-    super.key,
-  });
-
-  @override
-  Widget? desktop() {
-    return const _DesktopView();
-  }
-
-  @override
-  Widget? tablet() {
-    return const _TabletView();
-  }
-}
-
-class _DesktopView extends StatelessWidget {
-  const _DesktopView();
+class NoDataSection extends StatelessWidget {
+  const NoDataSection({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return _sectionBuilder(
-      context: context,
-      borderRadius: BorderRadius.circular(AppDecoration.radiusBig),
+    final ThemeData themeData = Theme.of(context);
+    final TextTheme textTheme = themeData.textTheme;
+
+    return CustomSection(
+      padding: const EdgeInsets.only(
+        top: AppDecoration.paddingLargest,
+        bottom: AppDecoration.paddingMedium,
+        right: AppDecoration.paddingBig,
+        left: AppDecoration.paddingBig,
+      ),
+      wrapSpacing: AppDecoration.spaceMedium,
+      layout: SectionLayout.wrap,
+      children: [
+        Column(
+          children: [
+            CustomText(
+              text: "1002@home".tr,
+              textAlign: TextAlign.center,
+              style: textTheme.displayMedium!.copyWith(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            verticalSpace(),
+            CustomText(
+              text: "1003@home".tr,
+              textAlign: TextAlign.center,
+              style: textTheme.titleLarge,
+              blueLightColor: true,
+              maxWidth: AppDecoration.pageWidth,
+            )
+          ],
+        ),
+      ],
     );
   }
-}
-
-class _TabletView extends StatelessWidget {
-  const _TabletView();
-
-  @override
-  Widget build(BuildContext context) {
-    return _sectionBuilder(
-      context: context,
-      textAlign: TextAlign.center,
-      crossAxisAlignment: CrossAxisAlignment.center,
-    );
-  }
-}
-
-CustomSection _sectionBuilder({
-  required BuildContext context,
-  BorderRadiusGeometry? borderRadius,
-  CrossAxisAlignment? crossAxisAlignment,
-  TextAlign? textAlign,
-}) {
-  final ThemeData themeData = Theme.of(context);
-  final ColorScheme colorScheme = themeData.colorScheme;
-  final TextTheme textTheme = themeData.textTheme;
-
-  return CustomSection(
-    borderRadius: borderRadius,
-    backgroundColor: colorScheme.primary,
-    wrapSpacing: AppDecoration.spaceMedium,
-    layout: SectionLayout.wrap,
-    children: [
-      Icon(
-        LineIcons.alternateShield,
-        size: 120.0,
-        color: colorScheme.onPrimary,
-      ),
-      Column(
-        crossAxisAlignment: crossAxisAlignment ?? CrossAxisAlignment.start,
-        children: [
-          CustomText(
-            text: "1002@home".tr,
-            textAlign: textAlign ?? TextAlign.start,
-            maxWidth: 650.0,
-            style: textTheme.displaySmall!.copyWith(
-              color: colorScheme.onPrimary,
-            ),
-          ),
-          verticalSpace(AppDecoration.spaceSmall),
-          CustomText(
-            text: "1003@home".tr,
-            textAlign: textAlign ?? TextAlign.start,
-            maxWidth: 600.0,
-            style: textTheme.titleLarge!.copyWith(
-              color: colorScheme.onPrimary,
-              fontWeight: FontWeight.w300,
-            ),
-          )
-        ],
-      ),
-    ],
-  );
 }
